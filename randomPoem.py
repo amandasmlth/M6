065d0cedd7792aeb0036814bfa3555fea1c136de
Amanda Smith <asmith2@bowdoin.edu>
2022-11-08 10:48:56 -0500

    Created random poem generator

--- randomPoem.py
+++ randomPoem.py
@@ -25,14 +25,21 @@ def read_poems(folder):
 
 def write_poem(): 
     word_bank = read_poems('poem_database')
-    print(word_bank)
+    words = []
+    
+    #change word bank list of lists into one flat, condensed list that contains all the words 
+    for sublist in word_bank: 
+        for word in sublist: 
+            words.append(word)
 
     lines = 10 
     words_per_line = 6
+    line = ""
     for i in range(lines): 
+        line = ""
         for j in range(words_per_line): 
-            random.choice(word_bank)
-
+            line += random.choice(words) + " "
+        print(line)
 
 write_poem()
 
